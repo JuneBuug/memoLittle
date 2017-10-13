70a3b405f5a256c616bb9ba12a0116dcef745282
JuneBuug <junee613@gmail.com>
2017-10-13 16:59:25 +0900

    테이블뷰 swipe to delete 추가

--- memoLittle/ViewController/LittleLineViewController.swift
+++ memoLittle/ViewController/LittleLineViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class LittleLineViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    var list = ["호이","후이","포이"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,12 +32,25 @@ class LittleLineViewController: UIViewController {
 
 extension LittleLineViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3;
+        return list.count;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
          let cell = tableView.dequeueReusableCell(withIdentifier: "LittleLineLikeTableViewCell",for: indexPath) as! LittleLineLikeTableViewCell
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.delete) {
+            
+            list.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            tableView.reloadData()
+        }
     }
 }
