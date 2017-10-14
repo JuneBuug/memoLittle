2d8fbc1b7f91eeddb115c6ef52806379c456ab0e
JuneBuug <junee613@gmail.com>
2017-10-14 17:36:32 +0900

    LittleLine View Controller에 Searchbar Controller추가

--- memoLittle/ViewController/LittleLineViewController.swift
+++ memoLittle/ViewController/LittleLineViewController.swift
@@ -12,11 +12,18 @@ class LittleLineViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     var list = ["뫄뫄","준킴","탐"]
+    
+    let searchController = UISearchController(searchResultsController: nil)
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.dataSource = self
         tableView.delegate = self
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
         
         tableView.register(UINib(nibName: "LittleLineLikeTableViewCell", bundle: nil), forCellReuseIdentifier: "LittleLineLikeTableViewCell")
         tableView.register(UINib(nibName: "LittleLineEventTableViewCell", bundle: nil), forCellReuseIdentifier: "LittleLineEventTableViewCell")
@@ -28,26 +35,37 @@ class LittleLineViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func isFiltering() -> Bool {
+        return searchController.isActive
+    }
+    
 
 }
 
 extension LittleLineViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count;
+        if isFiltering() {
+            return 5;
+        }else{
+            return list.count;
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if indexPath.row % 2 == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "LittleLineLikeTableViewCell",for: indexPath) as! LittleLineLikeTableViewCell
-            cell.personName.text? = list[indexPath.row]
-            return cell
+        if isFiltering(){
+            return UITableViewCell()
         }else{
-            let cell = tableView.dequeueReusableCell(withIdentifier: "LittleLineEventTableViewCell",for: indexPath) as! LittleLineEventTableViewCell
-            cell.personName.text? = list[indexPath.row]
-            return cell
+            if indexPath.row % 2 == 0 {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "LittleLineLikeTableViewCell",for: indexPath) as! LittleLineLikeTableViewCell
+                cell.personName.text? = list[indexPath.row]
+                return cell
+            }else{
+                let cell = tableView.dequeueReusableCell(withIdentifier: "LittleLineEventTableViewCell",for: indexPath) as! LittleLineEventTableViewCell
+                cell.personName.text? = list[indexPath.row]
+                return cell
+            }
         }
-        
     }
     
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -61,5 +79,11 @@ extension LittleLineViewController: UITableViewDelegate, UITableViewDataSource {
             tableView.deleteRows(at: [indexPath], with: .automatic)
             tableView.reloadData()
         }
+    }
+}
+
+extension LittleLineViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+     
     }
 }
