5b54a3ece8e4c0b181d5d0b429c020ed0665451a
JuneBuug <junee613@gmail.com>
2017-10-14 18:19:31 +0900

    searchbar 색 변경

--- memoLittle/Asset/AppDelegate.swift
+++ memoLittle/Asset/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UISearchBar.appearance().barTintColor = UIColor(red: 241.0/255.0,green: 241.0/255.0 ,blue: 239.0/255.0, alpha: 1.0)
+        UISearchBar.appearance().tintColor =  UIColor(red: 71.0/255.0,green: 71.0/255.0 ,blue: 71.0/255.0, alpha: 1.0)
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).tintColor =  UIColor(red: 241.0/255.0,green: 241.0/255.0 ,blue: 239.0/255.0, alpha: 1.0)
         return true
     }
 
