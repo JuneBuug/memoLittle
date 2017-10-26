48de481df636294e2e6e8bd0338277cab44bf7b9
JuneBuug <junee613@gmail.com>
2017-10-26 15:37:49 +0900

    설정 viewController 추가

---
+++ memoLittle/ViewController/SettingsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsViewController.swift
+//  memoLittle
+//
+//  Created by 준킴 on 2017. 10. 26..
+//  Copyright © 2017년 junebuug. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
