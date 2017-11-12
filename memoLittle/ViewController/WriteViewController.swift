1aae5d0db5a416bbae34fc163840a0a1fbed5f65
JuneBuug <junee613@gmail.com>
2017-11-12 16:24:34 +0900

    mention(@) 시 사람 이름 hightlight 기능 추가

--- memoLittle/ViewController/WriteViewController.swift
+++ memoLittle/ViewController/WriteViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RealmSwift
 
-class WriteViewController: UIViewController {
+class WriteViewController: UIViewController,UITextViewDelegate {
 
     @IBOutlet weak var textView: UITextView!
     var writer = Person()
@@ -26,11 +26,12 @@ class WriteViewController: UIViewController {
         let addButton = UIBarButtonItem(
             barButtonSystemItem: .done,
             target: self,
-            action: #selector(sth)
+            action: #selector(writeMemo)
         )
         addButton.tintColor = UIColor.black
         keyboardToolbar.items = [addButton]
         textView.inputAccessoryView = keyboardToolbar
+        textView.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -39,7 +40,8 @@ class WriteViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @objc func sth(){
+    
+    @objc func writeMemo(){
         let obj = LittleLine()
         obj.objectName = textView.text
         obj.personName = writer.name
@@ -53,6 +55,44 @@ class WriteViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 
+    func checkandReturnMention(text: String) -> (Bool,String){
+        
+        if text.contains("@") {
+            let textArr = text.split(separator: " ")
+            
+            for word in textArr {
+                if word.starts(with: "@"){
+                    if word.count > 1 {
+                        let name = word.dropFirst()
+                        return (true,String(name))
+                    }
+                }
+            }
+        }
+        return (false,"")
+    }
+    
+    func textViewDidChange(_ textView: UITextView){
+        
+        if checkandReturnMention(text: textView.text).0 {
+            let searchString = checkandReturnMention(text: textView.text).1
+            let baseString = textView.text!
+            
+            let attributed = NSMutableAttributedString(string: baseString)
+            let hightlightColor = UIColor(red: 0.0/255.0, green: 175.0/255.0, blue: 126.0/255.0, alpha: 1.0)
+            do
+            {
+                let regex = try! NSRegularExpression(pattern: searchString,options: .caseInsensitive)
+                for match in regex.matches(in: baseString, options: NSRegularExpression.MatchingOptions(), range: NSRange(location: 0, length: baseString.characters.count)) as [NSTextCheckingResult] {
+                    attributed.addAttribute(NSAttributedStringKey.foregroundColor, value: hightlightColor, range: match.range)
+                }
+                DispatchQueue.main.async{
+                    self.textView.attributedText = attributed
+                }
+            }
+        }
+      
+    }
     @IBAction func onTouchClose(_ sender: Any) {
         navigationController?.popViewController(animated: true)
         dismiss(animated: true, completion: nil)
